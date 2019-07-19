57a82032dfec831c826c3206c555aee5910ea131
louisGustavo <luis.gustavo_18@outlook.com>
2019-07-18 23:50:17 -0300

    Valores padrão, desestruturação, rest/spread

--- bundle.js
+++ bundle.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { keys.push.apply(keys, Object.getOwnPropertySymbols(object)); } if (enumerableOnly) keys = keys.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
@@ -19,7 +25,8 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 //----------------| DECLARAÇÃO DE CLASSES |----------------
-//método estático - não precisa instanciar numa variável
+console.log('-----| DECLARAÇÃO DE CLASSES |------'); //método estático - não precisa instanciar numa variável
+
 var Matematica =
 /*#__PURE__*/
 function () {
@@ -90,14 +97,15 @@ document.getElementById('novotodo').onclick = function () {
   MinhaLista.add('Novo todo');
 }; //MinhaLista.mostraUsuario();
 //----------------| VARIÁVEIS CONST E LET |----------------
-//const não é possível mudar seu valor
+
+
+console.log('-----| VARIÁVEIS CONST E LET |------'); //const não é possível mudar seu valor
 
 /*
 const a = 1
 a = 2;
 */
 //mas é possível mutar o valor do objeto
-
 
 var usuario = {
   nome: 'Luis'
@@ -115,6 +123,7 @@ function teste(x) {
 //-----------| TRABALHANDO COM ARRAYS E ARROW FUNCTIONS |-----------
 
 
+console.log('-----| TRABALHANDO COM ARRAYS E ARROW FUNCTIONS |------');
 var arr = [1, 3, 5, 8, 10]; //um dos meios de executar um foreach() em ES6
 
 var newArr = arr.map(function (item, index) {
@@ -156,7 +165,8 @@ var find = arr.find(function (item) {
   return item === 5;
 });
 console.log(find); //---------| DECLARAÇÃO DE FUNCTIONS EM VARIÁVEIS |---------
-//é possível criar uma function diretamente numa varíavel
+
+console.log('-----| DECLARAÇÃO DE FUNCTION EM VARIÁVEIS |------'); //é possível criar uma function diretamente numa varíavel
 
 var novoTeste = function novoTeste() {
   console.log('teste');
@@ -176,4 +186,92 @@ var newestTest = function newestTest() {
   };
 };
 
-console.log(newestTest());
+console.log(newestTest()); //valores padrão
+
+console.log('-----| VALORES PADRAO |------');
+/*
+function somar(a = 3, b = 5){
+	return a+b;
+}
+*/
+
+var somar = function somar() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 5;
+  return a + b;
+};
+
+console.log(somar()); //Desestruturação de objeto
+
+console.log('-----| DESESTRUTURAÇÃO DE OBJETO |------');
+var pessoa = {
+  nome: 'Luis',
+  idade: 29,
+  endereco: {
+    cidade: 'São Paulo',
+    estado: 'SP'
+  } //Com a desestrutução consegue extrair informações especificações de 
+  //um objeto
+  //const { nome, idade, endereco : { cidade } } = pessoa;
+  //console.log(nome, cidade);
+  //O mesmo vale para parâmetros de um método
+
+};
+
+function mostraNome(_ref) {
+  var nome = _ref.nome;
+  console.log(nome);
+}
+
+mostraNome(pessoa); //---------| OPERADORES REST/SPREAD |---------
+
+console.log('-----| OPERADORES REST/SPREAD |------'); //REST
+
+console.log('-| REST |-'); //Usando a desestruturação para obter informações específicas do código
+//pode-se usar o REST para armazenar numa variável o resto das informações
+//do objeto informado.
+
+var user = {
+  nome: 'Leandro',
+  idade: 23,
+  empresa: 'RocketSeat'
+}; //const { idade, ...resto } = user;
+//o mesmo vale para vetores (array)
+
+var vetor = [1, 2, 3, 4, 5];
+var a = vetor[0],
+    b = vetor[1],
+    c = vetor.slice(2);
+console.log(c); //e também para métodos
+//dessa forma não é necessário informar todo os parametros que o método precisa
+
+function executarSoma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+}
+
+console.log(executarSoma(1, 5, 7, 9)); //SPREAD
+
+console.log('-| SPREAD |-'); //pode ser utilizado para unificar informações de duas ou mais variáveis
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3); //ou criar uma segunda variável com as mesmas informações da primeira, porém
+//mudando alguns parâmetros
+
+var user1 = {
+  nome: 'Luis',
+  idade: 24
+}; //recupero as informações do usuário e só mudo o nome
+
+var user2 = _objectSpread({}, user1, {
+  nome: 'Gustavo'
+});
+
+console.log(user2);
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //----------------| DECLARAÇÃO DE CLASSES |----------------
-
+console.log('-----| DECLARAÇÃO DE CLASSES |------');
 //método estático - não precisa instanciar numa variável
 class Matematica{
 	static soma(a, b) {
@@ -39,7 +39,7 @@ document.getElementById('novotodo').onclick = function(){
 //MinhaLista.mostraUsuario();
 
 //----------------| VARIÁVEIS CONST E LET |----------------
-
+console.log('-----| VARIÁVEIS CONST E LET |------');
 //const não é possível mudar seu valor
 /*
 const a = 1
@@ -62,7 +62,7 @@ function teste(x){
 //teste(10);
 
 //-----------| TRABALHANDO COM ARRAYS E ARROW FUNCTIONS |-----------
-
+console.log('-----| TRABALHANDO COM ARRAYS E ARROW FUNCTIONS |------');
 const arr = [1, 3, 5, 8, 10];
 
 //um dos meios de executar um foreach() em ES6
@@ -111,7 +111,7 @@ const find = arr.find(item => item === 5);
 console.log(find);
 
 //---------| DECLARAÇÃO DE FUNCTIONS EM VARIÁVEIS |---------
-
+console.log('-----| DECLARAÇÃO DE FUNCTION EM VARIÁVEIS |------');
 //é possível criar uma function diretamente numa varíavel
 const novoTeste = () => {
 	console.log('teste');
@@ -126,3 +126,84 @@ console.log(newTest());
 const newestTest = () => ({nome : 'Otavio'});
 console.log(newestTest());
 
+//valores padrão
+console.log('-----| VALORES PADRAO |------');
+/*
+function somar(a = 3, b = 5){
+	return a+b;
+}
+*/
+const somar = (a = 3, b = 5) => a+b;
+
+console.log(somar());
+
+//Desestruturação de objeto
+console.log('-----| DESESTRUTURAÇÃO DE OBJETO |------');
+const pessoa = {
+	nome : 'Luis',
+	idade: 29,
+	endereco: {
+		cidade : 'São Paulo',
+		estado: 'SP'
+	}
+}
+//Com a desestrutução consegue extrair informações especificações de 
+//um objeto
+//const { nome, idade, endereco : { cidade } } = pessoa;
+//console.log(nome, cidade);
+
+//O mesmo vale para parâmetros de um método
+function mostraNome({nome}){
+	console.log(nome);
+}
+mostraNome(pessoa);
+
+//---------| OPERADORES REST/SPREAD |---------
+console.log('-----| OPERADORES REST/SPREAD |------');
+
+//REST
+console.log('-| REST |-');
+//Usando a desestruturação para obter informações específicas do código
+//pode-se usar o REST para armazenar numa variável o resto das informações
+//do objeto informado.
+const user = {
+	nome : 'Leandro',
+	idade : 23,
+	empresa : 'RocketSeat'
+};
+
+//const { idade, ...resto } = user;
+
+//o mesmo vale para vetores (array)
+const vetor = [1, 2, 3, 4, 5];
+const [ a, b, ...c ] = vetor;
+console.log(c);
+
+//e também para métodos
+//dessa forma não é necessário informar todo os parametros que o método precisa
+function executarSoma(...params){
+	return params.reduce((total, next) => total + next);
+}
+
+console.log(executarSoma(1, 5, 7, 9));
+
+//SPREAD
+console.log('-| SPREAD |-');
+
+//pode ser utilizado para unificar informações de duas ou mais variáveis
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [ ...arr1, ...arr2 ];
+
+console.log(arr3);
+
+//ou criar uma segunda variável com as mesmas informações da primeira, porém
+//mudando alguns parâmetros
+const user1 = {
+	nome : 'Luis',
+	idade : 24
+};
+
+//recupero as informações do usuário e só mudo o nome
+const user2 = { ...user1, nome : 'Gustavo' };
+console.log(user2);
